9a4a5405d685d5f50254ce1781c852f682c7bfca
BinhTX <binh.xuantran@digicommercegroup.com>
2024-11-05 16:17:24 +0700

    first request multi server

---
+++ src/main/java/com/binhcodev/inventory_service/controllers/InventoryRepository.java
@@ -0,0 +1,14 @@
+package com.binhcodev.inventory_service.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/inventories")
+public class InventoryRepository {
+    @GetMapping
+    public String getInventories() {
+        return "All inventories";
+    }
+}
